c09242571bc18efcecb83457c21681686be6fbd3
Ali Emir FIRINCIOĞULLARI <77262454+aliemirfrnc@users.noreply.github.com>
2023-01-12 19:18:51 +0300

    Create javacalisma5Soru.java

---
+++ javacalisma5Soru.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 1; i -= 3) {
+            //bu kısım döngünün başlangıç değerini, bitiş değerini ve artış değerini belirler. "i" değişkeni 100 ile başlar ve her döngüde 3 azaltılır. Böylece döngü 100'den başlar ve 1'e kadar devam eder.
+            System.out.println(i);
+            //Buradada 3'er 3'er azaltılan sayıları tek tek yazdırdık
+        }
+
+    }
+}
